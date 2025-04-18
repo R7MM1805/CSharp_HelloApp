a08b10e7261e56942aaf03a25a6f3b16eb28fab1
Ricardo Metzger <r7metzger@gmail.com>
2025-04-18 12:35:56 -0500

    feat: tipos y metodos comunes de string

---
+++ HelloApp/01-Bases/StringTypes.cs
@@ -0,0 +1,21 @@
+﻿namespace HelloApp._01_Bases
+{
+    partial class Program
+    {
+        public static void StringTypes()
+        {
+            string name = "Ricardo";
+            string message = "Hola " + name;
+            string interpolatedMessage = $"Hola {name}";
+            Console.WriteLine(message);
+            Console.WriteLine(interpolatedMessage);
+            Console.WriteLine($"Tu nombre tiene {name.Length} caracteres");
+            Console.WriteLine($"Tu nombre en mayusculas es {name.ToUpper()}");
+            Console.WriteLine($"Tu nombre en minuscula es {name.ToLower()}");
+            int number = 30;
+            Console.WriteLine(number.ToString());
+            bool isStudent = true;
+            Console.WriteLine(isStudent.ToString());
+        }
+    }
+}
--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -1,1 +1,1 @@
-﻿HelloApp._01_Bases.Program.NumericTypes();
+﻿HelloApp._01_Bases.Program.StringTypes();
