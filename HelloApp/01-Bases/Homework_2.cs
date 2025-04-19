bbea5f5c7c68107b290ce658425703614186a227
Ricardo Metzger <r7metzger@gmail.com>
2025-04-19 14:03:57 -0500

    feat: calculadora de salarios

---
+++ HelloApp/01-Bases/Homework_2.cs
@@ -0,0 +1,43 @@
+﻿namespace HelloApp._01_Bases
+{
+    partial class Program
+    {
+        /*
+         * Realice un programa que calcule el salario mensual de un trabajador
+         * Se debe pedir el nombre, horas trabajadas y tarifa por hora.
+         * El programa debe calcular el sueldo y mostrar los datos en consola.
+         */
+        public static void SalaryCalculator()
+        {
+            Console.WriteLine("Ingrese su nombre");
+            string? name = Console.ReadLine();
+            Console.WriteLine("Ingrese las horas trabajadas");
+            string? hours = Console.ReadLine();
+            Console.WriteLine("Ingrese su tarifa por hora");
+            string? salaryPerHour = Console.ReadLine();
+            string validation = ValidateInputs(name, hours, salaryPerHour);
+            string response = string.IsNullOrEmpty(validation) ? CalculateSalary(name!, int.Parse(hours!), decimal.Parse(salaryPerHour!)) : $"Error: {validation}";  
+            Console.WriteLine(response);
+        }
+        private static string ValidateInputs(string? name, string? hours, string? salaryPerHour)
+        {
+            string response = string.IsNullOrWhiteSpace(name) ? "Se requiere el nombre" : string.Empty;
+            response = string.IsNullOrEmpty(response) ? ValidateInt(hours) : response;
+            return string.IsNullOrEmpty(response) ? ValidateDecimal(salaryPerHour) : response;
+        }
+        private static string ValidateInt(string? hours) => int.TryParse(hours, out _) ? string.Empty : "Las horas ingresadas deben ser un número entero";
+        private static string ValidateDecimal(string? salaryPerHour) => decimal.TryParse(salaryPerHour, out _) ? string.Empty : "El salario por hora debe ser un decimal";
+        private static string CalculateSalary(string name, int hours, decimal salaryPerHour)
+        {
+            decimal totalSalary = hours * salaryPerHour;
+            return $"""
+                Calculadora de Salarios
+
+                Nombres: {name}
+                Horas trabajadas: {hours}
+                Sueldo por hora: {salaryPerHour:C}
+                Sueldo total: {totalSalary:C}
+                """;
+        }
+    }
+}
--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -1,1 +1,1 @@
-﻿HelloApp._01_Bases.Program.SalesReport();
+﻿HelloApp._01_Bases.Program.SalaryCalculator();
