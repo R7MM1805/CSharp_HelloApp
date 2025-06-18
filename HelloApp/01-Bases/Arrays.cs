d8f38ff4840a9d5e23f928bea75dfe203b67aa5f
Ricardo Metzger <r7metzger@gmail.com>
2025-06-17 23:41:55 -0500

    feat: rangos y sub arreglos

--- HelloApp/01-Bases/Arrays.cs
+++ HelloApp/01-Bases/Arrays.cs
@@ -5,20 +5,31 @@
         public static void Arrays()
         {
             /*Formas de inicializar arreglos*/
-
-            int[] numbers = new int[5];
-            numbers[0] = 1;
-            numbers[1] = 2;
-            numbers[2] = 3;
-            numbers[3] = 4;
-            numbers[4] = 5;
-
             int[] numbersArray = [1, 2, 3, 4, 5];
 
             /*Acceder a los datos del arreglo a traves de índices*/
             Console.WriteLine($"Primer elemento: {numbersArray[0]}");
             Console.WriteLine($"Tercer elemento: {numbersArray[2]}");
             Console.WriteLine($"Cantidad de elementos: {numbersArray.Length}");
+
+            /*Acceder al último elemento*/
+            Console.WriteLine($"Último elemento: {numbersArray[^1]}");
+            Console.WriteLine($"Penúltimo elemento: {numbersArray[^2]}");
+
+            /*Rango para obtener sub-arreglos*/
+            int[] firstThree = numbersArray[..3];
+            int[] fromIndexTwo = numbersArray[2..];
+
+            Console.WriteLine($"Primeros 3: {firstThree}");
+            foreach (int item in firstThree)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"Desde el segundo elemento: {fromIndexTwo}");
+            foreach (int item in fromIndexTwo)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
