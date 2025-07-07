828497b9678fdc24755c7ae2df6cdbc8ee053a3f
Ricardo Metzger <r7metzger@gmail.com>
2025-07-07 14:07:25 -0500

    feat: bucles - for - foreach

--- HelloApp/02-Logic/Loops.cs
+++ HelloApp/02-Logic/Loops.cs
@@ -6,7 +6,7 @@
         int counter = 1;
         while (counter <= 5)
         {
-            WriteLine($"Iteración: {counter}");
+            WriteLine($"Iteración While: {counter}");
             counter++;
         }
         #endregion
@@ -18,6 +18,30 @@
             WriteLine($"Número: {number}");
             number++;
         } while (number < 3);
+        #endregion
+
+        #region for
+        for (int i = 0; i < 10; i++)
+        {
+            WriteLine($"Iteración For: {i}");
+        }
+        for (int i = 10; i >= 0; i -= 2)
+        {
+            WriteLine($"Personalizar For: {i}");
+        }
+        #endregion
+
+        #region for-each
+        string[] fruits = ["Manzana", "Pera", "Uva", "Piña", "Granadilla"];
+        foreach (string fruit in fruits)
+        {
+            WriteLine($"Fruta Arreglo: {fruit}");
+        }
+        List<string> names = ["Ricardo", "Rosa", "Alvaro", "Martin", "Rosa", "Fabrizzio"];
+        foreach (string name in names)
+        {
+            WriteLine($"Nombre Lista: {name}");
+        }
         #endregion
     }
 }
