48978252c59e1610873ac6b1ef88338a2706e5da
Ricardo Metzger <r7metzger@gmail.com>
2025-07-07 14:01:12 -0500

    feat: bucles - while, do-while

---
+++ HelloApp/02-Logic/Loops.cs
@@ -0,0 +1,23 @@
+﻿partial class Program
+{
+    public static void Loops()
+    {
+        #region Bucle While
+        int counter = 1;
+        while (counter <= 5)
+        {
+            WriteLine($"Iteración: {counter}");
+            counter++;
+        }
+        #endregion
+
+        #region do-while
+        int number = 0;
+        do
+        {
+            WriteLine($"Número: {number}");
+            number++;
+        } while (number < 3);
+        #endregion
+    }
+}
