124e3cba99d27e79d74857f4458cdd2300eae92b
Ricardo Metzger <r7metzger@gmail.com>
2025-07-08 15:06:05 -0500

    feat: funcionaes anonimas y lambda

---
+++ HelloApp/02-Logic/AnonymusFunction.cs
@@ -0,0 +1,20 @@
+﻿partial class Program
+{
+    public static void AnonymusFunction()
+    {
+        WriteLine(square(15));
+        WriteLine(lambdaSquare(5));
+        WriteLine();
+        List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        List<int> evenNumber = [.. numbers.Where(x => x % 2 == 0)];
+        foreach (int even in evenNumber)
+        {
+            WriteLine(even);
+        }
+    }
+    static Func<int, int> square = delegate (int number)
+    {
+        return number * number;
+    };
+    static Func<int, int> lambdaSquare = x => x * x;
+}
