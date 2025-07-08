3a43a5f21746e902dda16bd8efb23f52b34618d1
Ricardo Metzger <r7metzger@gmail.com>
2025-07-08 14:19:29 -0500

    feat: conversion de fechas y culturas

---
+++ HelloApp/02-Logic/ConvertionToDateTime.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+partial class Program
+{
+    public static void ConvertionToDateTime()
+    {
+        CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("es-PE");
+
+        int friends = int.Parse("101");
+        double entryCost = 25.50;
+        DateTime birthDate = DateTime.Parse("2 Marzo 2025");
+        WriteLine($"Tengo {friends} amigos para invitar a mi fiesta");
+        WriteLine($"La celebración de mi cumpleaños será: {birthDate}");
+        WriteLine($"Formato largo: {birthDate:D}");
+        WriteLine($"El costo de la entrada será: {entryCost:C}");
+    }
+}
--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -1,1 +1,1 @@
-﻿Conversions();
+﻿ConvertionToDateTime();
