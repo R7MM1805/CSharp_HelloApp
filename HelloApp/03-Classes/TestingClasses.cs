5fefdbc10c552629daf7e7c60807459c1ffb30d7
Ricardo Metzger <r7metzger@gmail.com>
2025-08-15 18:37:25 -0500

    feat: clases en .net

---
+++ HelloApp/03-Classes/TestingClasses.cs
@@ -0,0 +1,23 @@
+﻿partial class Program
+{
+    public static void TestingClasses()
+    {
+        Vehicle toyota = new("Toyota", "Corolla", 2021);
+        toyota.ShowInformation();
+        Vehicle honda = new("Honda", "Civic", 2019);
+        honda.ShowInformation();
+        Vehicle renault = new("Renault", "Duster", 2024);
+        renault.ShowInformation();
+    }
+}
+class Vehicle(string brand, string model, int year)
+{
+    public string Brand { get; set; } = brand;
+    public string Model { get; set; } = model;
+    public int Year { get; set; } = year;
+
+    public void ShowInformation()
+    {
+        WriteLine($"Este vehículo es un {Brand} {Model} del año {Year}");
+    }
+}
--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -1,1 +1,1 @@
-﻿AnonymusFunction();
+﻿TestingClasses();
