c5806fdb1d578f2278669ad0bc506925add6355c
Ricardo Metzger <r7metzger@gmail.com>
2025-08-16 11:21:13 -0500

    feat: consultas simples linq

--- HelloApp/04-ExceptCollections/Linq.cs
+++ HelloApp/04-ExceptCollections/Linq.cs
@@ -15,11 +15,32 @@
             WriteLine(number);
         }
         WriteLine("Uso de LinQ");
-        WriteLine("Forma select");
+        WriteLine("Forma Querys");
         evenNumber = [.. from num in numbers where num % 2 == 0 select num];
         evenNumber.ForEach(WriteLine);
-        WriteLine("Forma Directa");
+        WriteLine("Forma Method");
         evenNumber = [.. numbers.Where(x => x % 2 == 0)];
         evenNumber.ForEach(WriteLine);
+
+        /* Consultas simples */
+        List<MarvelCharacter> characters =
+        [
+            new("Peter Parker", "Spider-Man", "Avengers"),
+            new("Tony Stark", "Iron Man", "Avengers"),
+            new("Steve Rogers", "Captain America", "Avengers"),
+            new("Natasha Romanoff", "Black Widow", "Avengers"),
+            new("T'Challa", "Black Panther", "Wakanda"),
+            new("Stephen Strange", "Doctor Strange", "Defenders")
+        ];
+        //Mostrar personajes del team Avengers
+        WriteLine("Personajes del team avengers");
+        List<string> avengersQuery = [.. characters.Where(x => x.Team == "Avengers").Select(x => $"{x.Name} - {x.Alias}")];
+        avengersQuery.ForEach(x => WriteLine(x.ToUpper()));
+    }
+    class MarvelCharacter(string name, string alias, string team)
+    {
+        public string Name { get; set; } = name;
+        public string Alias { get; set; } = alias;
+        public string Team { get; set; } = team;
     }
 }
