e79d9a675b7307824d5897950f96da2321cb26fb
Ricardo Metzger <r7metzger@gmail.com>
2025-08-16 08:13:05 -0500

    feat: clases abstractas

---
+++ HelloApp/03-Classes/AbstractClassExamples.cs
@@ -0,0 +1,32 @@
+﻿partial class Program
+{
+    public static void AbstractClassExamples()
+    {
+        HomeAppliance myWasher = new WashingMachine()
+        {
+            Brand = "Samsung"
+        };
+        HomeAppliance myMicrowave = new Microwave()
+        {
+            Brand = "LG"
+        };
+        myWasher.ShowBrand();
+        myWasher.TurnOn();
+        myMicrowave.ShowBrand();
+        myMicrowave.TurnOn();
+    }
+}
+abstract class HomeAppliance
+{
+    public string Brand { get; set; } = string.Empty;
+    public abstract void TurnOn();
+    public void ShowBrand() => WriteLine($"La marca del electrodoméstico es {Brand}");
+}
+class WashingMachine : HomeAppliance
+{
+    public override void TurnOn() => WriteLine($"La lavadora ha inicializado el ciclo de lavado");
+}
+class Microwave : HomeAppliance
+{
+    public override void TurnOn() => WriteLine($"El microondas está calentando la comida");
+}
