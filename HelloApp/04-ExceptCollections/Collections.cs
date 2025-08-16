dfe1f8be5479889d3ce92cc5222594cae36dacd4
Ricardo Metzger <r7metzger@gmail.com>
2025-08-16 10:50:32 -0500

    feat: diccionarios y hashset

--- HelloApp/04-ExceptCollections/Collections.cs
+++ HelloApp/04-ExceptCollections/Collections.cs
@@ -8,6 +8,46 @@
         names.Remove("Rosa");
         WriteLine("Despues de remover");
         ShowNames(names);
+        Dictionary<int, string> students = new()
+        {
+            { 1, "Ricardo" },
+            { 2, "Rosa" },
+            { 3, "Álvaro" },
+        };
+        WriteLine("Diccionario");
+        ShowStudents(students);
+        students.Add(4, "Maria");
+        WriteLine("Añadir datos a diccionario");
+        ShowStudents(students);
+        students.Remove(3);
+        WriteLine("Quitar datos a diccionario");
+        ShowStudents(students);
+        HashSet<string> uniqueNames = ["Álvaro", "Rosa", "Fabrizzio", "Maria"];
+        WriteLine("HashSet");
+        ShowUniqueNames(uniqueNames);
+        uniqueNames.Add("Ricardo");
+        uniqueNames.Add("Fabrizzio");
+        uniqueNames.Add("Martin");
+        uniqueNames.Add("Maria");
+        WriteLine("Añadir datos a HashSet");
+        ShowUniqueNames(uniqueNames);
+        uniqueNames.Remove("Fabrizzio");
+        WriteLine("Quitar datos a HashSet");
+        ShowUniqueNames(uniqueNames);
     }
     private static void ShowNames(List<string> names) => names.ForEach(WriteLine);
+    private static void ShowStudents(Dictionary<int, string> valuePairs)
+    {
+        foreach (KeyValuePair<int, string> student in valuePairs)
+        {
+            WriteLine($"Llave: {student.Key} - Valor: {student.Value}");
+        }
+    }
+    private static void ShowUniqueNames(HashSet<string> uniques)
+    {
+        foreach (string unique in uniques)
+        {
+            WriteLine(unique);
+        }
+    }
 }
